8e406491acb4768cdebba5ebb24958cb81f00bab
praveen <praveenravi4898@gmail.com>
2024-06-04 23:12:52 +0530

    homepage_created

---
+++ memory-game/src/pages/Instruction.tsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import plainbg from '../images/plainbg.png'
+import blueheart from "../images/blueheart.png"
+import apple from "../images/apple.png"
+
+const Instruction:React.FC = () => {
+  return (
+    <div style={{
+        backgroundImage:`url(${plainbg})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        width: '1512px', 
+        height: '100vh',
+        display:'flex',
+        justifyContent:"space-evenly"
+    }}>
+
+<div style={{borderRadius:"60px",marginTop:"180px",height:"338px",width:"351px",backgroundColor:"white",display:'flex',flexDirection:"column",justifyContent:"space-evenly",alignItems:"center"}}>
+
+
+<div style={{position: "relative"}}>
+<div style={{boxShadow:"rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",position: "absolute", top: "0",left: "0",transform: "rotate(-5.62deg)"}}>
+<div style={{display:"flex",justifyContent:"center",alignItems:"center", height:"150px",width:"135px",margin:"5px", background: 'linear-gradient(to right, #FBA8C1 0%, #FBA8C1 54%, #FBA8C1 97%)',borderRadius:"10px"}}>
+<img src={apple} alt="apple" />
+</div>
+</div>
+
+<div style={{boxShadow:"rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",position: "absolute", top: "0",left: "0"}}>
+<div style={{display:"flex",justifyContent:"center",alignItems:"center", height:"150px",width:"135px",margin:"5px", background: 'linear-gradient(to right, #FBA8C1 0%, #FBA8C1 54%, #FBA8C1 97%)',borderRadius:"10px"}}>
+<img src={apple} alt="apple" />
+</div>
+</div>
+
+
+</div>
+
+
+
+
+
+
+
+<div style={{height:"50px",width:"200px",textAlign:"center",fontSize:"35px",color:"#11AEC6",fontWeight:"800"}}>
+    Select a pink card
+</div>
+
+<div style={{height:"33px",width:"200px",textAlign:"center",fontSize:"24px",color:"#A6C930",fontWeight:"800"}}>
+    <p>It has images.</p>
+</div>
+
+</div>
+
+
+
+
+
+{/* secondcard */}
+<div style={{borderRadius:"60px",marginTop:"180px",height:"338px",width:"351px",backgroundColor:"white",display:'flex',flexDirection:"column",justifyContent:"space-evenly",alignItems:"center"}}>
+
+<div style={{boxShadow:"rgba(100, 100, 111, 0.2) 0px 7px 29px 0px"}}>
+<div style={{display:"flex",justifyContent:"center",alignItems:"center", height:"150px",width:"135px",margin:"5px", background: 'linear-gradient(#A2E3F3 0%, rgba(162, 227, 243, 0.29) 46%, #A2E3F3 100%)',borderRadius:"10px"}}>
+<img src={blueheart} alt="blueheart" />
+
+</div>
+</div>
+
+<div style={{height:"50px",width:"200px",textAlign:"center",fontSize:"35px",color:"#11AEC6",fontWeight:"800"}}>
+    Select a blue card
+</div>
+
+<div style={{height:"33px",width:"200px",textAlign:"center",fontSize:"24px",color:"#A6C930",fontWeight:"800"}}>
+    <p>It has alphabets.</p>
+</div>
+
+</div>
+
+
+{/* thirdcard */}
+
+<div style={{borderRadius:"60px",marginTop:"180px",height:"338px",width:"351px",backgroundColor:"white"}}></div>
+
+
+
+
+    </div>
+  )
+}
+
+export default Instruction
---
+++ memory-game/src/pages/Introscreen.tsx
@@ -0,0 +1,85 @@
+
+
+import React, { useState } from 'react';
+import introbg from '../images/introbackground.png';
+import textbox1 from '../images/textbox1.png';
+import startbutton from '../images/start.png';
+import nextbutton from '../images/next.png';
+import yesbutton from '../images/yess.png';
+
+const Introscreen: React.FC = () => {
+  const [textboxContent, setTextboxContent] = useState<string>('Welcome Kiddo !');
+  const [buttonImage, setButtonImage] = useState<string>(startbutton);
+  const [buttonText, setButtonText] = useState<string>('start');
+
+  const handleButtonClick = () => {
+    if (buttonText === 'start') {
+      setTextboxContent('Hi, I am Mizo! \nand I love bananas');
+      setButtonImage(nextbutton);
+      setButtonText('next');
+    } else if (buttonText === 'next') {
+      setTextboxContent('Can you help me get \n some ?');
+      setButtonImage(yesbutton);
+      setButtonText('yes');
+    } else if (buttonText === 'yes') {
+    }
+  };
+
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${introbg})`,
+        padding: '0',
+        margin: '0 auto',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '100vh',
+        width: '1512px',
+      }}
+    >
+      <img
+        style={{
+          height: '200px',
+          width: '500px',
+          marginTop: '30px',
+          marginLeft: '50px',
+          position: 'absolute',
+        }}
+        src={textbox1}
+        alt="textbox"
+      />
+      <h2
+        style={{
+          color: '#11AEC6',
+          fontSize: '40px',
+          fontWeight: '800',
+          marginTop: '60px',
+          marginLeft: '870px',
+          position: 'absolute',
+          whiteSpace: 'pre-line',
+        }}
+      >
+        {textboxContent}
+      </h2>
+      <button
+        onClick={handleButtonClick}
+        style={{
+          backgroundColor: 'transparent',
+          border: 'none',
+          textDecoration: 'none',
+          position: 'absolute',
+          marginTop: '550px',
+          marginLeft: '200px',
+        }}
+      >
+        <img
+          style={{ height: '100px', width: '250px' }}
+          src={buttonImage}
+          alt={buttonText + 'button'}
+        />
+      </button>
+    </div>
+  );
+};
+
+export default Introscreen;
